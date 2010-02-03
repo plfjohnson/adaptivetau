dc858470ba033541e14ba881f61ac15f460aaf2b
Philip Johnson <plfjohnson@github.com>
2010-02-03 15:13:34 +0000

    Added GPL to cpp source
    git-svn-id: file:///home/pjohnson/repo/prj/sim_algos/adaptivetau@141 26082704-6efc-0310-a1c6-a466a319e90f

--- src/adaptivetau.cpp
+++ src/adaptivetau.cpp
@@ -1,3 +1,27 @@
+/* $Id$
+    --------------------------------------------------------------------------
+    C++ implementation of the "adaptive tau-leaping" algorithm described by
+    Cao Y, Gillespie DT, Petzold LR. The Journal of Chemical Physics (2007).
+    Author: Philip Johnson <plfjohnson@emory.edu>
+
+
+    Copyright (C) 2010 Philip Johnson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    --------------------------------------------------------------------------
+*/
+
 #include <iostream>
 #include <vector>
 #include <limits>
